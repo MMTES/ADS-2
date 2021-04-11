8919e5f912ae7b03b17e28c17cd72d935ff6a871
MMTES <42440704+MMTES@users.noreply.github.com>
2021-04-11 22:08:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int countPairs1(int* arr, int len, int value) {
     int pairs = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                     pairs++;
@@ -15,7 +15,7 @@ int countPairs1(int* arr, int len, int value) {
 int countPairs2(int* arr, int len, int value) {
     int pairs = 0;
         for (int i = 0; i < len; i++) {
-                for (int j=len-1; j < len; j--) {
+                for (int j=len-1; j > len; j--) {
                         if (arr[i]+arr[j] == value)
                                 pairs++;
                 }
